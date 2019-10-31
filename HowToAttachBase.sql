9ab29a8816d06c30cf60805b20be7a57a186ae90
vit4er <vchru@yahoo.com>
2019-10-31 23:01:37 +0300

    Sql for attach db

---
+++ HowToAttachBase.sql
@@ -0,0 +1,4 @@
+﻿CREATE DATABASE airballsdb
+ON (FILENAME = N'd:\StudioProjects\Airballs\Airballs.API\App_Data\airballsdb.mdf'),
+(FILENAME = N'd:\StudioProjects\Airballs\Airballs.API\App_Data\airballsdb_Log.ldf')
+FOR ATTACH;
